72129b113f890ab63ffbeec406f0d618d4c4a596
Brody Bayley <brodybayley@gmail.com>
2021-02-23 23:39:18 +0000

    implement function to modify counter as user types

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,14 @@
-$(document).ready(function() {
-  console.log('Ready to go!');
+$(document).ready(() => {
+  console.log('this is from script js');
+  $('#tweet-text').on('keyup', onKeyUp);
 });
+
+const onKeyUp = function() {
+  const text = $(this).val();
+  const count = text.length;
+  console.log(count);
+  const output = $('.counter');
+  console.log(output);
+  const left = 140 - count;
+  output.html(left);
+};
