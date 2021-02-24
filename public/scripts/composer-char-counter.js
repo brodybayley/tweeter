abadfd8e329e9384de83192797f26a1b55f64320
Brody Bayley <brodybayley@gmail.com>
2021-02-24 02:11:45 +0000

    locate counter from js file using this

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,16 @@
-$(document).ready(() => {
+$(document).ready(function() {
   console.log('this is from script js');
+  //produces an event whenever user types in tweet box
   $('#tweet-text').on('keyup', onKeyUp);
 });
 
 const onKeyUp = function() {
   const text = $(this).val();
   const count = text.length;
-  console.log(count);
-  const output = $('.counter');
-  console.log(output);
+  //locates counter within index.html
+  const output = $(this).parent().find('.counter');
+  //subtracts user input from count
   const left = 140 - count;
+  //displays updated count
   output.html(left);
 };
