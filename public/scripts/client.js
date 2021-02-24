fe63d0a41f58e9b0244e8844fec1b570b4d18c07
Brody Bayley <brodybayley@gmail.com>
2021-02-24 21:49:14 +0000

    finish createTweetElement func. Modify tweet styling

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,49 +1,48 @@
-$(document).ready(() => {
-  const createTweetElement = function() {
-    const $tweet = $(`<article class="tweet">Hello world</article>`);
-
-  };
-
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  };
-
-
+$(document).ready(function() {
   const $tweet = createTweetElement(tweetData);
-
   console.log($tweet);
-  $('#tweet-container').append($tweet);
-
+  $('.old-tweets').append($tweet);
 });
 
-// const html = `
-//     < header class="tweet-header" >
-//     <div class="user-thumbnail">
-//       <span><img class="avatar-image" src="/images/profile-hex.png"></span>
-//       <span>Brody</span>
-//     </div>
-//     <span class="user-tagname">@BooBayley</span>
-//   </header>
-//   <div class="tweet-body">
-//     <span class="tweet-content">Hello World!</span>
-//   </div>
-//   <footer class="tweet-footer">
-//     <span class="tweet-age">10 days ago</span>
-//     <span class="links">
-//       <a href='#' class="tweet-icons">🏴</a>
-//       <a href='#' class="tweet-icons">🔁</a>
-//       <a href='#' class="tweet-icons">♥</a>
-//     </span>
-//   </footer>
-//   `;
+const createTweetElement = function(tweetData) {
+  const $tweet = $(`
+  <section class="old-tweets">
+      <article class="tweet-container">
+      <header class="tweet-header">
+  <div class="user-thumbnail">
+    <span><img class="avatar-image" src="${tweetData.user.avatars}"></span>
+    <span>${tweetData.user.name}</span>
+  </div>
+  <span class="user-tagname">${tweetData.user.handle}</span>
+</header>
+<div class="tweet-body">
+  <span class="tweet-content">${tweetData.content.text}</span>
+</div>
+<footer class="tweet-footer">
+  <span class="tweet-age">newDate${tweetData.created_at} days ago</span>
+  <span class="links">
+    <a href='#' class="tweet-icons">🏴</a>
+    <a href='#' class="tweet-icons">🔁</a>
+    <a href='#' class="tweet-icons">♥</a>
+  </span>
+</footer>
+</article>
+    </section>
+`);
+  return $tweet;
+};
 
-//     const tweetElement = $tweet.append(html);
-//     return tweetElement;
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1461116232227
+};
+
+
+
