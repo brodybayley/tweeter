0ee0adc9c6d7cdb32e3871d877c39fa5f977a001
Brody Bayley <brodybayley@gmail.com>
2021-02-24 20:58:27 +0000

    implement function to send hardcoded tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,14 +1,49 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+$(document).ready(() => {
+  const createTweetElement = function() {
+    const $tweet = $(`<article class="tweet">Hello world</article>`);
 
-const createTweetElement = function() {
-  const $tweet = $(`<article class="tweet">Hello world</article>`);
-};
+  };
 
-const $tweet = createTweetElement(tweetData);
+  const tweetData = {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  };
 
-console.log($tweet);
-$('#tweets-container').append($tweet);
+
+  const $tweet = createTweetElement(tweetData);
+
+  console.log($tweet);
+  $('#tweet-container').append($tweet);
+
+});
+
+// const html = `
+//     < header class="tweet-header" >
+//     <div class="user-thumbnail">
+//       <span><img class="avatar-image" src="/images/profile-hex.png"></span>
+//       <span>Brody</span>
+//     </div>
+//     <span class="user-tagname">@BooBayley</span>
+//   </header>
+//   <div class="tweet-body">
+//     <span class="tweet-content">Hello World!</span>
+//   </div>
+//   <footer class="tweet-footer">
+//     <span class="tweet-age">10 days ago</span>
+//     <span class="links">
+//       <a href='#' class="tweet-icons">🏴</a>
+//       <a href='#' class="tweet-icons">🔁</a>
+//       <a href='#' class="tweet-icons">♥</a>
+//     </span>
+//   </footer>
+//   `;
+
+//     const tweetElement = $tweet.append(html);
+//     return tweetElement;
